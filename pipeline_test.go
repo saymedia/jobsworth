ddcd7fc4ca25ba9edb0fd95b75d880af4f3c8f58
Yonathan Randolph <yonathan@gmail.com>
2022-06-11 08:34:25 -0700

    fix: Propagate HIL interpolation errors #5
    Propagate HIL (HashiCorp Interpolation Language) template errors from hil.Walk. Previously, jobsworth would silently ignore the error, leave the rest of the templates unevaluated, and upload the job anyway. Now, if any template evaluation returns an error, jobsworth will exit immediately with status 2.

--- pipeline_test.go
+++ pipeline_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 
 	"gopkg.in/yaml.v2"
@@ -37,7 +38,10 @@ func TestWaitStep(t *testing.T) {
 command: foo
 `)
 	yaml.Unmarshal(stepBytes, &step)
-	step = lowerStep(step, context, stepContext)
+	step, err := lowerStep(step, context, stepContext)
+	if err != nil {
+		t.Error("lowerStep returned err:", err)
+	}
 	if reflect.DeepEqual(step["env"], nil) {
 		t.Errorf("env should be set")
 	}
@@ -50,11 +54,53 @@ command: foo
 wait: ~
 `)
 	yaml.Unmarshal(stepBytes, &step)
-	step = lowerStep(step, context, stepContext)
+	step, err = lowerStep(step, context, stepContext)
+	if err != nil {
+		t.Error("lowerStep returned err:", err)
+	}
 	if !reflect.DeepEqual(step["env"], nil) {
 		t.Errorf("env should not be set for a wait step")
 	}
 	if !reflect.DeepEqual(step["agents"], nil) {
 		t.Errorf("agents should not be set for a wait step")
+	}
+}
+
+func TestInterpolate(t *testing.T) {
+	context := &Context{}
+	stepContext := &StepContext{
+		EnvironmentName: "myenv",
+	}
+
+	step := Step{}
+	stepBytes := []byte(`x: ${environment}`)
+	if err := yaml.Unmarshal(stepBytes, &step); err != nil {
+		t.Error("unmarshal error", err)
+	}
+	step, err := lowerStep(step, context, stepContext)
+	if err != nil {
+		t.Error("lowerStep returned err:", err)
+	}
+	actual := step["x"]
+	expected := "myenv"
+	if actual != expected {
+		t.Error("interpolate value does not match", actual, expected)
+	}
+}
+
+func TestInterpolateUnknownVariable(t *testing.T) {
+	context := &Context{}
+	stepContext := &StepContext{}
+	step := Step{}
+	stepBytes := []byte(`x: ${badvar}`)
+	if err := yaml.Unmarshal(stepBytes, &step); err != nil {
+		t.Error("unmarshal error", err)
+	}
+	step, err := lowerStep(step, context, stepContext)
+	if err == nil {
+		t.Error("lowerStep should error when a bad variable is referenced")
+	}
+	if !strings.Contains(err.Error(), "unknown variable accessed: badvar") {
+		t.Error("message should say something about bad varaible", err.Error())
 	}
 }
